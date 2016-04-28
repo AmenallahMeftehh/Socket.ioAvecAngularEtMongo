b2039d57b6876727d131eb5d0ee959ac112f131d
AmenallahMeftehh <meftah.amenallah@gmail.com>
2016-04-28 02:26:51 +0100

    finir l'application de chat coté frontend

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-var app = require('express')(),
-server = require('http').createServer(app),
-io=require('socket.io').listen(server);
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
 server.listen(3000);
+mongoose.connect('mongodb://localhost/chat')
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/bower_components'));
-io.sockets.on('connection',function(){
-console.log('working');
 
-});
+io.sockets.on('connection',function(socket){
+
+socket.on('send msg', function(data){
+  io.sockets.emit('get msg',data);
+})
+})
